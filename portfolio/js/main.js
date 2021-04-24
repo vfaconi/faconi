5246608d47826049e18015d17ae8ad926e6af580
vfaconi <vanessafaconi@gmail.com>
2021-04-24 20:19:09 -0300

    init

---
+++ portfolio/js/main.js
@@ -0,0 +1,35 @@
+//portfolio array
+const links = [
+    {
+        label: "week 1 notes",
+        url: "https://www.google.com.br"
+    }
+]
+    
+links.forEach(function(link){
+    //console.log(link)
+
+        //create element
+        let ol = document.createElement('ol');
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+       
+        
+        //set the content for each element
+        li.textContent = link.label;
+        a.setAttribute("href", link.url);
+        a.setAttribute("target", "_blank");
+        
+       
+        //put <li> with <a>     
+        a.appendChild(li);
+        //put <a> with <lo>
+        ol.appendChild(a);
+       
+        //display html
+        document.querySelector('div.list').appendChild(ol);
+   
+})
+
+
+
