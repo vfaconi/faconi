5352fb17b1d2c3b55630bc5d260184576cfa34a7
vfaconi <vanessafaconi@gmail.com>
2021-05-22 22:38:45 -0300

    week 5

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -18,7 +18,7 @@ const links = [
     },
     {
         label: "Week 5: notes",
-        url: "https://vfaconi.github.io/faconi/week5/notes.html"
+        url: "https://vfaconi.github.io/faconi/week5/index.html"
     }
 ];
    console.log(link);
