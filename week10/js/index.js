471cf5a53859f2304f3900c0034eab5dfb1e751e
vfaconi <38166675+vfaconi@users.noreply.github.com>
2021-06-24 23:38:25 -0300

    Update index.js

--- week10/js/index.js
+++ week10/js/index.js
@@ -1,6 +1,5 @@
-import {getJSON, getLocation} from "./utilities.js";
-
-const baseUrl ='https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02';
+import QuakesController from './QuakesController.js';
 
 const myQuakesController = new QuakesController('#quakeList');
 myQuakesController.init();
+
